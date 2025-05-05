14a57d9b3210970bf8f6b39d48131ccdac653e95
Kennyrae <kennedybatuboelijah@gmail.com>
2025-05-05 19:37:37 +0100

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -5,7 +5,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "HANSTZ$83vFtDC7",
+  SESSION_ID: process.env.SESSION_ID |HansTz&cW913DIJ#4ZP9cqeYl3liZ9-TUgaZmtJ3ej1x9DCXtYlBAZDmM6U| "HANSTZ$83vFtDC7",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
